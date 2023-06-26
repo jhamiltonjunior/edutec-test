5fece5a73f6267b55cace6427d49a3556592c245
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-06-26 20:11:36 -0300

    in frontend folder I added admin and patient folder

---
+++ frontend/patient/js/fecth.js
@@ -0,0 +1,97 @@
+const BASE_URL = 'http://localhost:8080'
+
+const loginOrCreateAccount = async (cpf, name) => {
+  const data = {
+    "cpf": `${cpf}`,
+    "name": `${name}`,
+  }
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(data)
+  }
+
+  try {
+    const response = await fetch(BASE_URL, params)
+    // console.log(await response.json())
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const searchSpecialty = async () => {
+  const params = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+  }
+
+  try {
+    const response = await fetch(`${BASE_URL}/specialty.php`, params)
+
+    // console.log(await response.json())
+
+    return response.json()
+  } catch (error) {
+    console.log(error, 'oi')
+  }
+}
+
+const searchProfessional = async (specialty) => {
+  const data = {
+    "specialty": `${specialty}`
+  }
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(data)
+  }
+
+  try {
+    const response = await fetch(`${BASE_URL}/professional.php`, params)
+
+    // console.log(await response.json())
+
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+const createGrade = async (cpf, professional, specialty, service, punctuality, frontDesk, infrastructure, feedback) => {
+  const data = {
+    "cpf": `${cpf}`,
+    "professional": `${professional}`,
+    "specialty": `${specialty}`,
+    "service": `${service}`,
+    "punctuality": `${punctuality}`,
+    "frontDesk": `${frontDesk}`,
+    "infrastructure": `${infrastructure}`,
+    "feedback": `${feedback}`
+  }
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;"
+    },
+    body: JSON.stringify(data)
+  }
+
+  try {
+    const response = await fetch(`${BASE_URL}/grade.php`, params)
+    console.log(await response.json())
+    return response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
