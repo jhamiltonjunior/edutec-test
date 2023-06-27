b2b12a2faa560643f676d98121708d2cf35368ca
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-06-27 19:05:38 -0300

    login file added in admin folder, but is in testing...

--- frontend/admin/js/fecth.js
+++ frontend/admin/js/fecth.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'http://localhost:8080'
 
-const loginOrCreateAccount = async (cpf, name) => {
+const login = async (cpf, password) => {
   const data = {
     "cpf": `${cpf}`,
-    "name": `${name}`,
+    "password": `${password}`,
   }
 
   const params = {
@@ -15,83 +15,83 @@ const loginOrCreateAccount = async (cpf, name) => {
   }
 
   try {
-    const response = await fetch(BASE_URL, params)
-    // console.log(await response.json())
-    return response.json()
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const searchSpecialty = async () => {
-  const params = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-  }
-
-  try {
-    const response = await fetch(`${BASE_URL}/specialty.php`, params)
-
-    // console.log(await response.json())
-
-    return response.json()
-  } catch (error) {
-    console.log(error, 'oi')
-  }
-}
-
-const searchProfessional = async (specialty) => {
-  const data = {
-    "specialty": `${specialty}`
-  }
-
-  const params = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify(data)
-  }
-
-  try {
-    const response = await fetch(`${BASE_URL}/professional.php`, params)
-
-    // console.log(await response.json())
-
-    return response.json()
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-const createGrade = async (cpf, professional, specialty, service, punctuality, frontDesk, infrastructure, feedback) => {
-  const data = {
-    "cpf": `${cpf}`,
-    "professional": `${professional}`,
-    "specialty": `${specialty}`,
-    "service": `${service}`,
-    "punctuality": `${punctuality}`,
-    "frontDesk": `${frontDesk}`,
-    "infrastructure": `${infrastructure}`,
-    "feedback": `${feedback}`
-  }
-
-  const params = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;"
-    },
-    body: JSON.stringify(data)
-  }
-
-  try {
-    const response = await fetch(`${BASE_URL}/grade.php`, params)
+    const response = await fetch(`${BASE_URL}/admin/login.php`, params)
     console.log(await response.json())
-    return response.json()
+    // return response.json()
   } catch (error) {
     console.log(error)
   }
 }
+
+// const searchSpecialty = async () => {
+//   const params = {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json; charset=utf-8"
+//     },
+//   }
+
+//   try {
+//     const response = await fetch(`${BASE_URL}/specialty.php`, params)
+
+//     // console.log(await response.json())
+
+//     return response.json()
+//   } catch (error) {
+//     console.log(error, 'oi')
+//   }
+// }
+
+// const searchProfessional = async (specialty) => {
+//   const data = {
+//     "specialty": `${specialty}`
+//   }
+
+//   const params = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json; charset=utf-8"
+//     },
+//     body: JSON.stringify(data)
+//   }
+
+//   try {
+//     const response = await fetch(`${BASE_URL}/professional.php`, params)
+
+//     // console.log(await response.json())
+
+//     return response.json()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+
+// const createGrade = async (cpf, professional, specialty, service, punctuality, frontDesk, infrastructure, feedback) => {
+//   const data = {
+//     "cpf": `${cpf}`,
+//     "professional": `${professional}`,
+//     "specialty": `${specialty}`,
+//     "service": `${service}`,
+//     "punctuality": `${punctuality}`,
+//     "frontDesk": `${frontDesk}`,
+//     "infrastructure": `${infrastructure}`,
+//     "feedback": `${feedback}`
+//   }
+
+//   const params = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json;"
+//     },
+//     body: JSON.stringify(data)
+//   }
+
+//   try {
+//     const response = await fetch(`${BASE_URL}/grade.php`, params)
+//     console.log(await response.json())
+//     return response.json()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
--- frontend/admin/js/main.js
+++ frontend/admin/js/main.js
@@ -1,11 +1,9 @@
 const cpf = document.querySelector('#cpf')
-const userName = document.querySelector('#name')
+const userPassword = document.querySelector('#password')
 
 const sendData = document.querySelector('.button')
 const invalidCPF = document.querySelector('.invalid-cpf')
 const invalidPassword = document.querySelector('.invalid-password')
-const specialty = document.querySelector('.specialty')
-const specialtyID = document.querySelector('#specialty')
 
 const formLogin = document.querySelector('#login')
 
@@ -23,23 +21,19 @@ formLogin.addEventListener('submit', async event => {
     invalidCPF.style.display = 'inline-block'
   }
 
-  if (userName.value.length <= 0 || userName.value.length > 255) {
-    userName.style.borderColor = '#fd0000'
+  if (userPassword.value.length <= 0 || userPassword.value.length > 255) {
+    userPassword.style.borderColor = '#fd0000'
     invalidName.style.display = 'inline-block'
   }
 
   if (
-    cpf.value.length === 11 && userName.value.length > 0
+    cpf.value.length === 11 && userPassword.value.length > 0
   ) {
-    const response = loginOrCreateAccount(cpf.value, userName.value)
-
-    // console.log(response)
+    const response = login(cpf.value, userPassword.value)
 
     cleanForm()
 
-    // console.log(await response)
-
-    const firstName = userName.value.split(' ')[0]
+    const firstName = userPassword.value.split(' ')[0]
 
     if (await response === 'Account created') {
       const text = document.createTextNode(`
@@ -59,8 +53,6 @@ formLogin.addEventListener('submit', async event => {
       `)
 
     welcomeUser(text)
-
-    createSelectSpecialtyElement()
   }
 
 })
@@ -73,173 +65,173 @@ const welcomeUser = (newAccount) => {
   specialty.appendChild(h2)
 }
 
-const createSelectSpecialtyElement = async () => {
+// const createSelectSpecialtyElement = async () => {
 
-  const response = await searchSpecialty()
+//   const response = await searchSpecialty()
 
-  // console.log(response)
+//   // console.log(response)
 
-  const labelSpecialty = document.createElement('label')
-  labelSpecialty.appendChild(document.createTextNode('Especialidade'))  
+//   const labelSpecialty = document.createElement('label')
+//   labelSpecialty.appendChild(document.createTextNode('Especialidade'))  
 
-  const selectSpecialty = document.createElement('select')
-  selectSpecialty.classList.add('select-specialty')
+//   const selectSpecialty = document.createElement('select')
+//   selectSpecialty.classList.add('select-specialty')
   
   
 
 
 
-  response.map((_, index, array) => {
-    const option = document.createElement('option')
+//   response.map((_, index, array) => {
+//     const option = document.createElement('option')
 
-    const name = document.createTextNode(`${array[index][1]}`)
+//     const name = document.createTextNode(`${array[index][1]}`)
 
-    option.appendChild(name)
+//     option.appendChild(name)
 
-    option.classList.add('button')
-    option.classList.add('button-specialty')
-
-
-    selectSpecialty.appendChild(option)
-  })
-
-  specialty.appendChild(labelSpecialty)
-  specialty.appendChild(selectSpecialty)
+//     option.classList.add('button')
+//     option.classList.add('button-specialty')
 
 
-  selectSpecialty.addEventListener('click', async (event) => {
+//     selectSpecialty.appendChild(option)
+//   })
 
-    // if (document.querySelector('.remove-it')) {
-    //   console.log('oi')
-    //   document.querySelectorAll('.remove-it').forEach(
-    //     (value) => value.remove()
-    //   )
-    // }
-
-    const professional = await searchProfessional(event.target.value)
-
-    const selectProfessional = document.querySelector('.select-professional')
-    selectProfessional.disabled = false
-
-    while (selectProfessional.firstChild) {
-      selectProfessional.firstChild.remove()
-    }
-
-    professional.map((_, index, array) => {
-      const option = document.createElement('option')
-
-      const arrOfPrfessional = array[index][0]
-
-      // console.log(arrOfPrfessional)
-      // console.log(arrOfPrfessional !== undefined)
-
-      if (arrOfPrfessional !== undefined) {
-        const name = document.createTextNode(`${arrOfPrfessional[1]}`)
-
-        option.appendChild(name)
-
-        option.classList.add('button')
-        option.classList.add('button-specialty')
+//   specialty.appendChild(labelSpecialty)
+//   specialty.appendChild(selectSpecialty)
 
 
-        selectProfessional.appendChild(option)
-      }
-    })
+//   selectSpecialty.addEventListener('click', async (event) => {
+
+//     // if (document.querySelector('.remove-it')) {
+//     //   console.log('oi')
+//     //   document.querySelectorAll('.remove-it').forEach(
+//     //     (value) => value.remove()
+//     //   )
+//     // }
+
+//     const professional = await searchProfessional(event.target.value)
+
+//     const selectProfessional = document.querySelector('.select-professional')
+//     selectProfessional.disabled = false
+
+//     while (selectProfessional.firstChild) {
+//       selectProfessional.firstChild.remove()
+//     }
+
+//     professional.map((_, index, array) => {
+//       const option = document.createElement('option')
+
+//       const arrOfPrfessional = array[index][0]
+
+//       // console.log(arrOfPrfessional)
+//       // console.log(arrOfPrfessional !== undefined)
+
+//       if (arrOfPrfessional !== undefined) {
+//         const name = document.createTextNode(`${arrOfPrfessional[1]}`)
+
+//         option.appendChild(name)
+
+//         option.classList.add('button')
+//         option.classList.add('button-specialty')
+
+
+//         selectProfessional.appendChild(option)
+//       }
+//     })
 
 
 
 
-  })
+//   })
 
-  const selectProfessional = document.createElement('select')
-  const labelProfessional = document.createElement('label')
+//   const selectProfessional = document.createElement('select')
+//   const labelProfessional = document.createElement('label')
 
 
-  selectProfessional.classList.add('select-professional')
+//   selectProfessional.classList.add('select-professional')
 
-  selectProfessional.required = true
-  selectProfessional.disabled = true
+//   selectProfessional.required = true
+//   selectProfessional.disabled = true
   
-  labelProfessional.appendChild(document.createTextNode('Profissional'))
+//   labelProfessional.appendChild(document.createTextNode('Profissional'))
   
-  specialty.appendChild(labelProfessional)
-  specialty.appendChild(selectProfessional)
+//   specialty.appendChild(labelProfessional)
+//   specialty.appendChild(selectProfessional)
 
-  const titles = ['Atendimento (1/10)', 'Pontualidade (1/10)', 'Recepção (1/10)', 'Infraestrutura (1/10)']
-  const classNames = ['punctuality', 'service', 'front-desk', 'infrastructure']
+//   const titles = ['Atendimento (1/10)', 'Pontualidade (1/10)', 'Recepção (1/10)', 'Infraestrutura (1/10)']
+//   const classNames = ['punctuality', 'service', 'front-desk', 'infrastructure']
 
-  for (let i = 0; i < titles.length; i++) {
-    const inputRange = document.createElement('input')
-    const label = document.createElement('label')
+//   for (let i = 0; i < titles.length; i++) {
+//     const inputRange = document.createElement('input')
+//     const label = document.createElement('label')
 
-    inputRange.classList.add('remove-it')
-    label.classList.add('remove-it')
+//     inputRange.classList.add('remove-it')
+//     label.classList.add('remove-it')
 
-    inputRange.type = 'range'
-    inputRange.min = 1
-    inputRange.max = 10
-    inputRange.step = 1
+//     inputRange.type = 'range'
+//     inputRange.min = 1
+//     inputRange.max = 10
+//     inputRange.step = 1
 
-    inputRange.value = 1
-    inputRange.classList.add(`${classNames[i]}`)
-
-
-    label.appendChild(document.createTextNode(`${titles[i]}`))
-
-    specialty.appendChild(label)
-    specialty.appendChild(inputRange)
-  }
-
-  const labelTextarea = document.createElement('label')
-
-  specialty.appendChild(labelTextarea)
-  labelTextarea.appendChild(document.createTextNode('Feedback'))
-
-  const textarea = document.createElement('textarea')
-
-  textarea.placeholder = 'Deixe o seu feedback'
-  textarea.classList.add('textarea')
-  textarea.required = true
-
-  specialty.appendChild(textarea)
+//     inputRange.value = 1
+//     inputRange.classList.add(`${classNames[i]}`)
 
 
-  const sendFeedback = document.createElement('button')
+//     label.appendChild(document.createTextNode(`${titles[i]}`))
 
-  sendFeedback.innerHTML = 'Pronto'
-  sendFeedback.classList.add('button')
-  sendFeedback.classList.add('button-specialty')
+//     specialty.appendChild(label)
+//     specialty.appendChild(inputRange)
+//   }
 
-  specialty.appendChild(sendFeedback)
-}
+//   const labelTextarea = document.createElement('label')
+
+//   specialty.appendChild(labelTextarea)
+//   labelTextarea.appendChild(document.createTextNode('Feedback'))
+
+//   const textarea = document.createElement('textarea')
+
+//   textarea.placeholder = 'Deixe o seu feedback'
+//   textarea.classList.add('textarea')
+//   textarea.required = true
+
+//   specialty.appendChild(textarea)
+
+
+//   const sendFeedback = document.createElement('button')
+
+//   sendFeedback.innerHTML = 'Pronto'
+//   sendFeedback.classList.add('button')
+//   sendFeedback.classList.add('button-specialty')
+
+//   specialty.appendChild(sendFeedback)
+// }
 
 
 
-specialty.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const feedback = document.querySelector('.textarea')
-  const selectProfessional = document.querySelector('.select-professional')
-  const selectSpecialty = document.querySelector('.select-specialty')
+// specialty.addEventListener('submit', (event) => {
+//   event.preventDefault()
+//   const feedback = document.querySelector('.textarea')
+//   const selectProfessional = document.querySelector('.select-professional')
+//   const selectSpecialty = document.querySelector('.select-specialty')
   
-  const service = document.querySelector('.service').value
-  const punctuality = document.querySelector('.punctuality').value
-  const frontDesk = document.querySelector('.front-desk').value
-  const infrastructure = document.querySelector('.infrastructure').value
+//   const service = document.querySelector('.service').value
+//   const punctuality = document.querySelector('.punctuality').value
+//   const frontDesk = document.querySelector('.front-desk').value
+//   const infrastructure = document.querySelector('.infrastructure').value
 
-  const specialtyValue = selectSpecialty.options[selectSpecialty.selectedIndex].value
-  if (selectProfessional.length !== 0){
-    const professionalValue = selectProfessional.options[selectProfessional.selectedIndex].value
+//   const specialtyValue = selectSpecialty.options[selectSpecialty.selectedIndex].value
+//   if (selectProfessional.length !== 0){
+//     const professionalValue = selectProfessional.options[selectProfessional.selectedIndex].value
     
-    createGrade(
-      cpf.value, professionalValue, specialtyValue, Number(service), Number(punctuality), Number(frontDesk), Number(infrastructure), String(feedback.value)
-    )
-  } else {
-    alert('Escolha o profissional!')
-  }
+//     createGrade(
+//       cpf.value, professionalValue, specialtyValue, Number(service), Number(punctuality), Number(frontDesk), Number(infrastructure), String(feedback.value)
+//     )
+//   } else {
+//     alert('Escolha o profissional!')
+//   }
 
 
 
-})
+// })
 
 const cleanForm = () => {
   formLogin.style.display = 'none'
