89e140a23dfacc9242f64238ad1ca9c26a3c6825
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-06-29 22:25:48 -0300

    filter almost complete, missing only the data range

--- backend/admin/assessment/filter.php
+++ backend/admin/assessment/filter.php
@@ -15,11 +15,10 @@ if (isset($_POST)) {
   $grade_interval_max = $mysqli->real_escape_string($decode['gradeIntervalMax']);
   $professional = $mysqli->real_escape_string($decode['professional']);
 
-
-  $sql_code = "SELECT * FROM grade WHERE ";
+  $sql_code = "SELECT * FROM grade WHERE";
   
   if ($grade_interval_min && $grade_interval_max) {
-    $sql_code .= " BETWEEN $grade_interval_min AND $grade_interval_max AND";
+    $sql_code .= " average BETWEEN $grade_interval_min AND $grade_interval_max AND";
   }
   
   if ($professional) {
@@ -30,11 +29,17 @@ if (isset($_POST)) {
     $sql_code .= " specialty_id = $specialty AND";
   } 
   
-  if ($initial_date && $finaly_date) {
-    $sql_code .= " created_at BETWEEN $initial_date AND $finaly_date AND";
-  }
-  
-  $sql_code.= " order by grade_id";
+  // if ($initial_date && $finaly_date) {
+  //   $sql_code .= " created_at BETWEEN $initial_date AND $finaly_date AND";
+  // }
 
-  $sql_query = $mysqli->query($sql_code) or die("Fail in code SQL: " . $mysqli->error);
+  $new_sql_code = rtrim($sql_code, "AND");
+
+  $new_sql_code .= " ORDER BY grade_id";
+  
+  $sql_query = $mysqli->query($new_sql_code) or die("Fail in code SQL: " . $mysqli->error);
+
+  echo json_encode($sql_query->fetch_all());
 }
+
+// SELECT * FROM grade WHERE average BETWEEN 1 AND 2 AND professional_id = 6 AND specialty_id = 4 AND created_at BETWEEN 2023-06-29 AND 2023-06-29
